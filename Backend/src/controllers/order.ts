2a4832580a99a22be3ad39c471665d2f8ac78c61
Vishal Kumar <imvgupta21@gmail.com>
2024-03-15 02:20:26 +0530

    Order Req Body's Type Created

--- Backend/src/controllers/order.ts
+++ Backend/src/controllers/order.ts
@@ -1,3 +1,7 @@
+import { Request } from "express";
 import { TryCatch } from "../middlewares/error.js";
+import { NewOrderRequestBody } from "../types/types.js";
 
-export const newOrder = TryCatch(async () => {});
+export const newOrder = TryCatch(
+  async (req: Request<{}, {}, NewOrderRequestBody>, res, next) => {}
+);
--- Backend/src/types/types.ts
+++ Backend/src/types/types.ts
@@ -46,3 +46,30 @@ export type InvalidateCacheProps = {
   order?: boolean;
   admin?: boolean;
 };
+
+export type OrderItemType = {
+  name: string;
+  photo: string;
+  price: number;
+  quantity: number;
+  productId: string;
+};
+
+export type ShippingInfoType = {
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+  pinCode: number;
+};
+
+export interface NewOrderRequestBody {
+  shippingInfo: ShippingInfoType;
+  user: string;
+  subtotal: number;
+  tax: number;
+  shippingCharges: number;
+  discount: number;
+  total: number;
+  orderItems: OrderItemType[];
+}
