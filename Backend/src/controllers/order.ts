f127e09809291d06aa0af81e8ab26319264b965e
Vishal Kumar <imvgupta21@gmail.com>
2024-04-05 23:42:36 +0530

    My Orders, Admin Orders, Get Single Order API functionality implemented

--- Backend/src/controllers/order.ts
+++ Backend/src/controllers/order.ts
@@ -4,6 +4,7 @@ import { NewOrderRequestBody } from "../types/types.js";
 import { Order } from "../models/order.js";
 import { invalidateCache, reduceStock } from "../utils/features.js";
 import ErrorHandler from "../utils/utility-class.js";
+import { myCache } from "../app.js";
 
 export const newOrder = TryCatch(
   async (req: Request<{}, {}, NewOrderRequestBody>, res, next) => {
@@ -42,3 +43,56 @@ export const newOrder = TryCatch(
     });
   }
 );
+
+export const myOrders = TryCatch(async (req, res, next) => {
+  const { id: user } = req.query;
+  const key = `my-orders-${user}`;
+
+  let orders = [];
+  if (myCache.has(key)) orders = JSON.parse(myCache.get(key) as string);
+  else {
+    orders = await Order.find({ user });
+    myCache.set(key, JSON.stringify(orders));
+  }
+
+  return res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+export const allOrders = TryCatch(async (req, res, next) => {
+  const key = `all-orders`;
+
+  let orders = [];
+  if (myCache.has(key)) orders = JSON.parse(myCache.get(key) as string);
+  else {
+    orders = await Order.find().populate("user", "name");
+    myCache.set(key, JSON.stringify(orders));
+  }
+
+  return res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+export const getSingleOrder = TryCatch(async (req, res, next) => {
+  const { id } = req.params;
+  const key = `order-${id}`;
+  let order;
+
+  if (myCache.has(key)) order = JSON.parse(myCache.get(key) as string);
+  else {
+    order = await Order.findById(id).populate("user", "name");
+
+    if (!order) return next(new ErrorHandler("Order Not Found", 404));
+
+    myCache.set(key, JSON.stringify(order));
+  }
+
+  return res.status(200).json({
+    success: true,
+    order,
+  });
+});
--- Backend/src/routes/order.ts
+++ Backend/src/routes/order.ts
@@ -1,10 +1,23 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.js";
-import { newOrder } from "../controllers/order.js";
+import {
+  allOrders,
+  getSingleOrder,
+  myOrders,
+  newOrder,
+} from "../controllers/order.js";
 
 const app = express.Router();
 
 // route - /api/v1/order/new
 app.post("/new", newOrder);
+
+// route - /api/v1/order/my
+app.get("/my", myOrders);
+
+// route - /api/v1/order/all
+app.get("/all", adminOnly, allOrders);
+
+app.route("/:id").get(getSingleOrder);
 
 export default app;
