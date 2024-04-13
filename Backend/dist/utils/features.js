51c89f4c99b3992f5a1d930b63c8b747753b03e3
Vishal Kumar <imvgupta21@gmail.com>
2024-04-13 23:02:44 +0530

    Bar Chart API: CalculatePercent Functionality Implemented

--- Backend/dist/utils/features.js
+++ Backend/dist/utils/features.js
@@ -47,7 +47,7 @@ export const reduceStock = async (orderItems) => {
 export const calculatePercentage = (thisMonth, lastMonth) => {
     if (lastMonth === 0)
         return thisMonth * 100;
-    const percent = ((thisMonth - lastMonth) / lastMonth) * 100;
+    const percent = (thisMonth / lastMonth) * 100;
     return Number(percent.toFixed(0));
 };
 export const getInventories = async ({ categories, productsCount, }) => {
--- Backend/src/utils/features.ts
+++ Backend/src/utils/features.ts
@@ -58,7 +58,7 @@ export const reduceStock = async (orderItems: OrderItemType[]) => {
 
 export const calculatePercentage = (thisMonth: number, lastMonth: number) => {
   if (lastMonth === 0) return thisMonth * 100;
-  const percent = ((thisMonth - lastMonth) / lastMonth) * 100;
+  const percent = (thisMonth / lastMonth) * 100;
   return Number(percent.toFixed(0));
 };
 
