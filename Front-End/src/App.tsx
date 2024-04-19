b8328880ce5c1da214d26f7e44808a013ff8bd44
Vishal Kumar <imvgupta21@gmail.com>
2024-04-19 18:30:19 +0530

    userReducer implemented for redux: state management

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,9 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
 
 import Loader from "./components/loader";
 import Header from "./components/header";
 import { Toaster } from "react-hot-toast";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+import { userExist, userNotExist } from "./redux/reducer/userReducer";
+import { useDispatch } from "react-redux";
+import { getUser } from "./redux/api/userAPI";
 
 const Home = lazy(() => import("./pages/home"));
 const Search = lazy(() => import("./pages/search"));
@@ -33,6 +38,17 @@ const TransactionManagement = lazy(
 );
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const data = await getUser(user.uid);
+        dispatch(userExist(data.user));
+      } else dispatch(userNotExist());
+    });
+  }, []);
+
   return (
     <Router>
       <Header />
